9a2a322e3b3a1fcaf387472064a9d56d75d77474
max <max@koritskiy.ru>
2013-09-26 10:39:35 +0400

    Add another method to initialization

--- lib/google-contacts-w-avatar/auth/user_login.rb
+++ lib/google-contacts-w-avatar/auth/user_login.rb
@@ -22,9 +22,8 @@ module GoogleContacts
       end
 
       def google_contacts(args={})
-        args = @init_args.merge(args)
         unless @not_init_google_contacts || @google_contacts.present?
-          @google_contacts = GoogleContacts::Client.new(access_token: @token, user_email: init_args[:email] || @email, auth_header: "GoogleLogin auth=#{@token}", gdata_version: '2.0', reconnect: args[:reconnect] || @reconnect )
+          init_google_contacts!(args)
           @google_contacts.client = self
         end
         @google_contacts
@@ -36,6 +35,13 @@ module GoogleContacts
         @token = @client.clientlogin(username || @username, password || @password, args[:captcha_token], args[:captcha_answer], args[:service], args[:account_type])
         @client
       end
+
+      private
+        def init_google_contacts!(args={})
+          args = @init_args.merge(args)
+          @google_contacts = GoogleContacts::Client.new(access_token: @token, user_email: init_args[:email] || @email, auth_header: "GoogleLogin auth=#{@token}", gdata_version: '2.0', reconnect: args[:reconnect] || @reconnect )
+        end
+
     end
   end
 end
