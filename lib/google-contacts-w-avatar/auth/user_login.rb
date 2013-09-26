ad8598a80ce8b82a1805b38cf4d2cfe65004e3dd
max <max@koritskiy.ru>
2013-09-26 10:22:54 +0400

    Add all auth args

--- lib/google-contacts-w-avatar/auth/user_login.rb
+++ lib/google-contacts-w-avatar/auth/user_login.rb
@@ -2,7 +2,7 @@ module GoogleContacts
   module Auth
     class UserLogin
 
-      attr_accessor :client, :token, :username, :password, :email, :reconnect, :not_init_google_contacts
+      attr_accessor :client, :token, :username, :password, :email, :reconnect, :not_init_google_contacts, :init_args
 
       def initialize(username, password, args={})
         @email=args[:email]
@@ -10,6 +10,7 @@ module GoogleContacts
         @username = username
         @password = password
         @email = email
+        @init_args = args
         auth(username, password, args)
       end
 
@@ -20,17 +21,19 @@ module GoogleContacts
         @google_contacts = object
       end
 
-      def google_contacts
+      def google_contacts(args={})
+        args = @init_args.merge(args)
         unless @not_init_google_contacts || @google_contacts.present?
-          @google_contacts = GoogleContacts::Client.new(access_token: token, user_email: args[:email] || @email, auth_header: "GoogleLogin auth=#{@token}", gdata_version: '2.0', reconnect: args[:reconnect] || @reconnect )
+          @google_contacts = GoogleContacts::Client.new(access_token: @token, user_email: init_args[:email] || @email, auth_header: "GoogleLogin auth=#{@token}", gdata_version: '2.0', reconnect: args[:reconnect] || @reconnect )
           @google_contacts.client = self
         end
         @google_contacts
       end
 
       def auth(username = nil, password = nil, args={})
+        args = @init_args.merge(args)
         @client = GData::Client::Contacts.new
-        @token = @client.clientlogin(username || @username, password || @password)
+        @token = @client.clientlogin(username || @username, password || @password, args[:captcha_token], args[:captcha_answer], args[:service], args[:account_type])
         @client
       end
     end
