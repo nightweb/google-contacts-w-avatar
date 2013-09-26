0f47a43898030c43889b73bdefc14d5b45a01639
max <max@koritskiy.ru>
2013-09-26 09:57:28 +0400

    Fix for reconnect

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 # Overview
 
 ## 0.0.2
+
+   - Added support ClientLogin authorization mechanize
+   - Added get/upload pictures functional
--- lib/google-contacts-w-avatar/auth/user_login.rb
+++ lib/google-contacts-w-avatar/auth/user_login.rb
@@ -2,9 +2,11 @@ module GoogleContacts
   module Auth
     class UserLogin
 
-      attr_accessor :client, :token, :google_contacts, :username, :password, :email
+      attr_accessor :client, :token, :username, :password, :email
 
-      def initialize(username, password, email=nil)
+      def initialize(username, password, args={})
+        email=args[:email]
+        reconnect=args[:reconnect]
         @username = username
         @password = password
         @email = email
@@ -14,7 +16,8 @@ module GoogleContacts
       def auth(username = nil, password = nil, email = nil)
         @client = GData::Client::Contacts.new
         @token = @client.clientlogin(username || @username, password || @password)
-        @google_contacts = GoogleContacts::Client.new(access_token: token, user_email: email || @email, auth_header: "GoogleLogin auth=#{@token}", gdata_version: '2.0' )
+        @google_contacts = GoogleContacts::Client.new(access_token: token, user_email: email || @email, auth_header: "GoogleLogin auth=#{@token}", gdata_version: '2.0', reconnect: reconnect )
+        @google_contacts.client = self
         @google_contacts
       end
 
